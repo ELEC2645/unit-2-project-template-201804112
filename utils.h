e43d7c771cd193d51f84b513f67e8ddd827f1836
201804112 <el23fz@leeds.ac.uk>
2025-11-27 00:53:33 +0000

    upload

--- utils.h
+++ utils.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 
-// 声明通用辅助函数
+// Declaration of Utilities Functions
 void strip_newline(char *str);
 void get_safe_string(const char *prompt, char *buffer, int size);
 double get_safe_double(const char *prompt);
